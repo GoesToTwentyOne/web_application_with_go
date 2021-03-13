85be5ebdd38af8bd3ff8cca0bc580cfd35b67734
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-13 12:18:00 +0600

    method 1

--- 02_3_page_website_in_golang/main.go
+++ 02_3_page_website_in_golang/main.go
@@ -10,6 +10,7 @@ func main() {
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/features", features)
+	http.HandleFunc("/request", request)
 	http.HandleFunc("/docs", docs)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
 	http.ListenAndServe(":8888", nil)
@@ -56,5 +57,14 @@ func docs(response http.ResponseWriter, req *http.Request) {
 	ptmp.Execute(response, nil)
 
 	// fmt.Fprintf(response, "Hello my home page")
+
+}
+func request(response http.ResponseWriter, req *http.Request) {
+	//method name
+	name := req.FormValue("name")
+	company := req.FormValue("company")
+	email := req.FormValue("email")
+	fmt.Println(name, company, email)
+	fmt.Fprintf(response, "Received %s %s %s", name, company, email)
 
 }
