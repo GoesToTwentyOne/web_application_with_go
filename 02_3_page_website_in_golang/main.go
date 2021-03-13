18cec366ec61ce14055a034c7838bf24104b72c9
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-13 23:14:45 +0600

    try faild

--- 02_3_page_website_in_golang/main.go
+++ 02_3_page_website_in_golang/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -14,6 +17,24 @@ func main() {
 	http.HandleFunc("/docs", docs)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
 	http.ListenAndServe(":8888", nil)
+}
+func init() {
+	fmt.Println("Go MySQL Tutorial")
+
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err := sql.Open("mysql", "root:Alex@tcp(127.0.0.1:3306)/hosting_db")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+	fmt.Println("connection Ok")
 }
 func home(response http.ResponseWriter, req *http.Request) {
 	ptmp, err := template.ParseFiles("template/base.gohtml")
@@ -60,11 +81,17 @@ func docs(response http.ResponseWriter, req *http.Request) {
 
 }
 func request(response http.ResponseWriter, req *http.Request) {
-	//method name
-	name := req.FormValue("name")
-	company := req.FormValue("company")
-	email := req.FormValue("email")
-	fmt.Println(name, company, email)
-	fmt.Fprintf(response, "Received %s %s %s", name, company, email)
+	//method 1 name
+	// name := req.FormValue("name")
+	// company := req.FormValue("company")
+	// email := req.FormValue("email")
+	// fmt.Println(name, company, email)
+	// fmt.Fprintf(response, "Received %s %s %s", name, company, email)
+	//method 2
+	req.ParseForm()
+	for key, value := range req.Form {
+		fmt.Println(key, value)
+	}
+	fmt.Fprintf(response, "OK")
 
 }
