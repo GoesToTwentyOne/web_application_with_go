c3819114faae715b78ee9ba16fde8ffce9bee97b
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-14 15:42:26 +0600

    database connection and data insert

--- 02_3_page_website_in_golang/main.go
+++ 02_3_page_website_in_golang/main.go
@@ -5,10 +5,33 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+var err error
+
+func init() {
+	// fmt.Println("Go MySQL Tutorial")
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err = sql.Open("mysql", "root:Alex@tcp(127.0.0.1:3306)/hosting_db")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	// defer db.Close()
+
+	fmt.Println("connection Ok")
+}
 func main() {
 
 	http.HandleFunc("/", home)
@@ -18,24 +41,7 @@ func main() {
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
 	http.ListenAndServe(":8888", nil)
 }
-func init() {
-	fmt.Println("Go MySQL Tutorial")
 
-	// Open up our database connection.
-	// I've set up a database on my local machine using phpmyadmin.
-	// The database is called testDb
-	db, err := sql.Open("mysql", "root:Alex@tcp(127.0.0.1:3306)/hosting_db")
-
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// defer the close till after the main function has finished
-	// executing
-	defer db.Close()
-	fmt.Println("connection Ok")
-}
 func home(response http.ResponseWriter, req *http.Request) {
 	ptmp, err := template.ParseFiles("template/base.gohtml")
 	if err != nil {
@@ -82,16 +88,25 @@ func docs(response http.ResponseWriter, req *http.Request) {
 }
 func request(response http.ResponseWriter, req *http.Request) {
 	//method 1 name
-	// name := req.FormValue("name")
-	// company := req.FormValue("company")
-	// email := req.FormValue("email")
+	name := req.FormValue("name")
+	company := req.FormValue("company")
+	email := req.FormValue("email")
 	// fmt.Println(name, company, email)
 	// fmt.Fprintf(response, "Received %s %s %s", name, company, email)
 	//method 2
-	req.ParseForm()
-	for key, value := range req.Form {
-		fmt.Println(key, value)
+	// req.ParseForm()
+	// for key, value := range req.Form {
+	// 	fmt.Println(key, value)
+	// }
+
+	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
+	sql := fmt.Sprintf(qs, name, company, email)
+	//fmt.Println(sql)
+	insert, err := db.Query(sql)
+	if err != nil {
+		panic(err.Error())
 	}
+	defer insert.Close()
 	fmt.Fprintf(response, "OK")
 
 }
